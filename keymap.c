864ae73a59f634d52dbc9dee6ae385693f00c2ea
Christian Rieger <christian.rieger@student.tugraz.at>
2024-07-16 22:48:23 +0200

    [master] updated keyboard layout

--- keymap.c
+++ keymap.c
@@ -15,22 +15,22 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_CTLA, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
+    KC_CTLA, MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LGUI, KC_F),    KC_G,  KC_H,    MT(MOD_RGUI, KC_J),    MT(MOD_RCTL, KC_K),    MT(MOD_RALT, KC_L),   KC_SCLN,
     KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RLSH,
-                    LT(2, KC_ENT), GUI_T(KC_SPC),   CTL_T(KC_BSPC), LT(1, KC_ESC) 
+                    LT(2, KC_TAB), GUI_T(KC_SPC),   CTL_T(KC_BSPC), LT(1, KC_ESC) 
   ),
 
   [1] = LAYOUT(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ENT,
+    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TILD,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  
   ),
 
   [2] = LAYOUT(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, QK_BOOT,
+    KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_QUOT,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TILD,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
