20e9d4f6a2546c0598b6d8b053eaa0fb1905bc1d
Christian Rieger <christian.rieger@student.tugraz.at>
2024-07-19 23:09:14 +0200

    [master] remove testing key from layout, formatted layers a bit

--- keymap.c
+++ keymap.c
@@ -14,17 +14,17 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_CTLA, MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LGUI, KC_F),    KC_G,  KC_H,    MT(MOD_RGUI, KC_J),    MT(MOD_RCTL, KC_K),    MT(MOD_RALT, KC_L),   KC_SCLN,
-    KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RLSH,
-                    LT(2, KC_TAB), KC_SPC,   KC_BSPC, LT(1, KC_ESC) 
+    KC_Q,    KC_W,               KC_E,               KC_R,               KC_T,                              KC_Y, KC_U,               KC_I,               KC_O,               KC_P,
+    KC_CTLA, MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LGUI, KC_F), KC_G,                              KC_H, MT(MOD_RGUI, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_RALT, KC_L), KC_SCLN,
+    KC_LSHZ, KC_X,               KC_C,               KC_V,               KC_B,                              KC_N, KC_M,               KC_COMM,            KC_DOT,             KC_RLSH,
+                                     LT(2, KC_TAB), KC_SPC,    KC_BSPC, LT(1, KC_ESC) 
   ),
 
   [1] = LAYOUT(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, CW_TOGG,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TILD,
-                                    MO(3), KC_TRNS, KC_DEL, KC_TRNS  
+    KC_1   ,    KC_2, KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,      KC_9,    KC_0,
+    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, CW_TOGG,                  KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, KC_ENT ,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,                  KC_HOME, KC_PGDN, KC_PGUP,    KC_END, KC_TILD,
+                                          MO(3), KC_TRNS, KC_DEL, KC_TRNS  
   ),
 
   [2] = LAYOUT(
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-    KC_F11,  KC_F12,  KC_TRNS, C_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
