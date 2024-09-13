90b9aed2760b8134d0e100b59e8ff06c4d1b5412
Christian Rieger <christian.rieger@student.tugraz.at>
2024-09-13 23:41:50 +0200

    [keymap-changes] add the keymap changes from zmk formware

--- keymap.c
+++ keymap.c
@@ -3,6 +3,18 @@
 
 #include QMK_KEYBOARD_H
 
+// Tap Dance declarations
+enum {
+    TD_Y_ESC,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_Y_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_ESC),
+};
+
+
 #define KC_CTSC RCTL_T(KC_SCLN)
 #define KC_CTLA LCTL_T(KC_A)
 #define KC_LSHZ LSFT_T(KC_Z)
@@ -14,22 +26,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_Q,    KC_W,               KC_E,               KC_R,               KC_T,                              KC_Y, KC_U,               KC_I,               KC_O,               KC_P,
-    KC_CTLA, MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LGUI, KC_F), KC_G,                              KC_H, MT(MOD_RGUI, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_RALT, KC_L), KC_SCLN,
-    KC_LSHZ, KC_X,               KC_C,               KC_V,               KC_B,                              KC_N, KC_M,               KC_COMM,            KC_DOT,             KC_RLSH,
-                                     LT(2, KC_TAB), KC_SPC,    KC_BSPC, LT(1, KC_ESC) 
+    KC_Q         ,KC_W              , KC_E              , KC_R,               KC_T,        TD(TD_Y_ESC), KC_U              , KC_I              , KC_O               , KC_P,
+    LSFT_T(KC_A) ,MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LGUI, KC_F), KC_G,        KC_H, MT(MOD_RGUI, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_RALT, KC_L) , RSFT_T(KC_SCLN),
+    KC_Z         ,KC_X              , KC_C              , KC_V,               KC_B,        KC_N, KC_M              , KC_COMM           , KC_DOT             , KC_SLSH,
+                                       LT(2, KC_TAB), KC_SPC,    KC_ENT, LT(1, KC_BSPC) 
   ),
 
   [1] = LAYOUT(
     KC_1   ,    KC_2, KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,      KC_9,    KC_0,
-    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, CW_TOGG,                  KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, KC_ENT ,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,                  KC_HOME, KC_PGDN, KC_PGUP,    KC_END, KC_TILD,
-                                          MO(3), KC_TRNS, KC_DEL, KC_TRNS  
+    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, CW_TOGG,                  KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, KC_TRNS ,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,                  KC_HOME, KC_PGDN, KC_PGUP,   KC_END , KC_TRNS,
+                                          MO(3), KC_ESC, KC_TRNS, KC_DEL  
   ),
 
   [2] = LAYOUT(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_TAB,  KC_TRNS, KC_TRNS, KC_DQT, KC_QUOT,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_DQT, KC_QUOT,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_GRV, KC_TILD,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                                     KC_TRNS, KC_TRNS, KC_TRNS, MO(3)
   ),
